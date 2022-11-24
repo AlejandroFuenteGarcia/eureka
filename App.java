7bade577c1a25da69eab11e4afee7efb4f2509a6
Alejandro Fuente Garcia <afuengar2001@gmail.com>
2022-11-24 12:07:18 +0100

    Incluyo cambios

--- App.java
+++ App.java
@@ -1,9 +1,12 @@
 public class App {
   
   public static void main(String[] args) {
+    System.out.println("Alejandro Fuente");
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("24/11/2022");
+    System.out.println("San Andrés de Rabanedo");
   }
   
 }
